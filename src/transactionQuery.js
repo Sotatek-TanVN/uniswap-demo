fabec10709e6f74881755c2b3e0bd9bb1f7d80ab
thanh.nguyen <thanh.nguyen3@sotatek.com>
2021-03-11 11:02:20 +0700

    get-transactions

---
+++ src/transactionQuery.js
@@ -0,0 +1,29 @@
+import gql from 'graphql-tag'
+
+export const FILTERED_TRANSACTIONS = gql`
+  {
+    swaps(first: 1, orderBy: timestamp, orderDirection: desc) {
+      transaction {
+        id
+        timestamp
+      }
+      id
+      pair {
+        token0 {
+          id
+          symbol
+        }
+        token1 {
+          id
+          symbol
+        }
+      }
+      amount0In
+      amount0Out
+      amount1In
+      amount1Out
+      amountUSD
+      to
+    }
+  }
+`
